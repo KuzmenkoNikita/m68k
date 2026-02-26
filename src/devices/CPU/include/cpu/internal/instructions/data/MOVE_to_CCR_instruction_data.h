82f2267cd8940593c56e42cfb6f066ee051c9ad7
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-02-26 20:20:56 +0300

    Added MOVE to CCR instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/MOVE_to_CCR_instruction_data.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "addressing_mode_instruction_data.h"
+#include <variant>
+
+namespace m68k::InstructionData {
+
+
+struct MOVE_to_CCR_InstructionData {
+
+    std::variant<DataRegisterModeData,
+                AddressModeData,
+                AddressWithPostincrementModeData,
+                AddressWithPredecrementModeData,
+                AddressWithDisplacementModeData,
+                AddressWithIndexModeData,
+                AbsoluteShortModeData,
+                AbsoluteLongModeData,
+                ImmediateModeData,
+                ProgramCounterWithDisplacementModeData,
+                ProgramCounterWithIndexModeData>  adressingModeData;
+};
+
+
+} //namespace m68k::InstructionData
