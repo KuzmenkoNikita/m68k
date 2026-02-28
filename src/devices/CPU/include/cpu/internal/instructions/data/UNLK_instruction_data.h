f1198c0fa229c2d7197aee170881f2c17053ef18
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-02-28 12:35:37 +0300

    Added UNLK instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/UNLK_instruction_data.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+namespace m68k::InstructionData {
+
+
+struct UNLK_InstructionData {
+
+    uint8_t addrRegisterNumber;
+};
+
+
+} //namespace m68k::InstructionData
