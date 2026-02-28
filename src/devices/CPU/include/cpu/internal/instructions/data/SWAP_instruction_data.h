beda6bdf41769f2300349ef090d209d74fdc439c
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-02-28 11:54:40 +0300

    Added swap instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/SWAP_instruction_data.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+
+namespace m68k::InstructionData {
+
+
+struct SWAP_InstructionData {
+
+    uint8_t dataRegNumber;
+
+};
+
+
+} //namespace m68k::InstructionData
