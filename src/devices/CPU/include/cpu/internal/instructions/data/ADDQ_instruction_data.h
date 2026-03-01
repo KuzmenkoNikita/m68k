665852f1ca2515eb8ea67f93f388af0ef8b60bb7
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-03-01 12:57:00 +0300

    Added ADDQ instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/ADDQ_instruction_data.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "addressing_mode_instruction_data.h"
+#include <cpu/internal/instructions/instruction_params.h>
+#include <variant>
+
+namespace m68k::InstructionData {
+
+
+struct ADDQ_InstructionData {
+
+    OperationSize size;
+
+    uint8_t data;
+
+    std::variant<DataRegisterModeData,
+                AddressRegisterModeData,
+                AddressModeData,
+                AddressWithPostincrementModeData,
+                AddressWithPredecrementModeData,
+                AddressWithDisplacementModeData,
+                AddressWithIndexModeData,
+                AbsoluteShortModeData,
+                AbsoluteLongModeData>  addressingModeData;
+
+};
+
+
+} //namespace m68k::InstructionData
