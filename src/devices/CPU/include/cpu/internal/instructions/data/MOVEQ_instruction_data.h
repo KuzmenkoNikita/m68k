a8d0e7385c5d371989caa50c916f84b9a2a50833
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-03-02 20:39:32 +0300

    Added MOVEQ instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/MOVEQ_instruction_data.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdint>
+
+
+namespace m68k::InstructionData {
+
+struct MOVEQ_InstructionData {
+
+    uint8_t dataRegNumber;
+    int8_t data;
+
+}; 
+
+
+} //namespace m68k::InstructionData
