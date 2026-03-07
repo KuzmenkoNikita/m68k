2a0023e711b26d8fcd8fcfae26c6c41b8fc1cf44
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-03-07 11:15:52 +0300

    Added ABCD instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/ABCD_instruction_data.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cpu/internal/instructions/instruction_params.h>
+#include <cstdint>
+
+namespace m68k::InstructionData {
+
+struct ABCD_InstructionData {
+    OperandAddressingMode operandAddressingMode;
+    uint8_t destinationRegister;
+    uint8_t sourceRegister;
+
+}; 
+
+
+} //namespace m68k::InstructionData
