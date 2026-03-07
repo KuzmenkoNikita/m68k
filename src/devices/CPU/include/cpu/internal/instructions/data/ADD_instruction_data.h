7904bd146b5b882e4a0e65936b187931128d56b7
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-03-07 16:31:14 +0300

    Added ADD instruction data

---
+++ src/devices/CPU/include/cpu/internal/instructions/data/ADD_instruction_data.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "addressing_mode_instruction_data.h"
+#include <cpu/internal/instructions/instruction_params.h>
+#include <variant>
+
+namespace m68k::InstructionData {
+
+
+struct ADD_InstructionData {
+
+    DestinationOperandType destOperandType;
+    OperationSize size;
+    uint8_t dataRegisterNumber;
+
+    std::variant<DataRegisterModeData,
+            AddressRegisterModeData,
+            AddressModeData,
+            AddressWithPostincrementModeData,
+            AddressWithPredecrementModeData,
+            AddressWithDisplacementModeData,
+            AddressWithIndexModeData,
+            AbsoluteShortModeData,
+            AbsoluteLongModeData,
+            ImmediateModeData,
+            ProgramCounterWithDisplacementModeData,
+            ProgramCounterWithIndexModeData> addressingModeData;
+};
+
+
+} //namespace m68k::InstructionData
