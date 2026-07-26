d0d2d752852803ddbb9f46337feb7ec6195c6430
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-07-26 15:53:28 +0000

    DIVU decoder

---
+++ src/devices/CPU/src/decoders/DIVU_decoder.cpp
@@ -0,0 +1,61 @@
+#include <bus_helper/bus_helper.h>
+#include <expected>
+#include <instruction_decoder/decoders/DIVU_decoder.h>
+#include <instruction_decoder/decoders/decoders_helpers.h>
+
+namespace m68k::decoders_ {
+
+namespace {
+
+constexpr uint16_t REGISTER_MASK = 0b111;
+constexpr uint16_t MODE_MASK = 0b11'1000;
+constexpr uint16_t DATA_REG_MASK = 0b1110'0000'0000;
+
+
+} //namespace
+
+DIVU_Decoder::DIVU_Decoder(std::shared_ptr<DataExchange::MemoryInterface> bus) : bus_(std::move(bus))
+{
+
+}
+
+std::expected<DecodeResult, DecodeError> DIVU_Decoder::decode(uint16_t opcodeWord, uint32_t instructionStartAddr) const
+{
+    InstructionData::DIVU_InstructionData instructionData{};
+
+    instructionData.dataRegNumber = (opcodeWord & DATA_REG_MASK) >> 9U; //NOLINT
+
+    const uint8_t registerValue = opcodeWord & REGISTER_MASK;
+    const uint8_t modeValue = (opcodeWord & MODE_MASK) >> 3U; //NOLINT
+
+    const auto addressingMode = getAddressingMode(modeValue, registerValue);
+    if(!addressingMode) {
+        return std::unexpected(addressingMode.error());
+    }
+
+    GetAddressingModeDataParams getAddressingModeParams {
+        .opSize = OperationSize::WORD,
+        .addressingMode = addressingMode.value(),
+        .registerValue = registerValue,
+        .addressingModeDataStartAddr = static_cast<uint32_t>(instructionStartAddr + sizeof(opcodeWord))
+    };
+
+    const auto addressingModeData = getAddressingModeData(*bus_, getAddressingModeParams);
+    if(!addressingModeData) {
+        return std::unexpected(addressingModeData.error());
+    }
+
+    const auto convertedAddressingModeData = convertAddressingModeData<InstructionData::DIVU_InstructionData::AddressingModeData>(addressingModeData->data);
+    if(!convertedAddressingModeData) {
+        return std::unexpected(DecodeError::INVALID_ADDRESSING_MODE);
+    }
+
+    instructionData.addressingModeData = *convertedAddressingModeData;
+
+    return DecodeResult {
+        .instruction = instructionData,
+        .instructionSizeBytes = static_cast<uint32_t>(addressingModeData.value().bytesReaded + sizeof(opcodeWord))
+    };
+}
+
+} // namespace m68k::decoders_
