c7513e6d74d414071b81de6104dad4874cf1a538
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-08-07 16:29:36 +0000

    MULU decoder

--- src/devices/CPU/include/cpu/internal/instructions/data/MULU_instruction_data.h
+++ src/devices/CPU/include/cpu/internal/instructions/data/MULU_instruction_data.h
@@ -7,19 +7,19 @@ namespace m68k::InstructionData {
 
 struct MULU_InstructionData {
 
+    using AddressingModeData = std::variant<DataRegisterModeData,
+                                            AddressModeData,
+                                            AddressWithPostincrementModeData,
+                                            AddressWithPredecrementModeData,
+                                            AddressWithDisplacementModeData,
+                                            AddressWithIndexModeData,
+                                            AbsoluteShortModeData,
+                                            AbsoluteLongModeData,
+                                            ImmediateModeData,
+                                            ProgramCounterWithDisplacementModeData,
+                                            ProgramCounterWithIndexModeData>;
+    AddressingModeData sourceOperand;
     uint8_t destinationDataRegister;
-
-    std::variant<DataRegisterModeData,
-                AddressModeData,
-                AddressWithPostincrementModeData,
-                AddressWithPredecrementModeData,
-                AddressWithDisplacementModeData,
-                AddressWithIndexModeData,
-                AbsoluteShortModeData,
-                AbsoluteLongModeData,
-                ImmediateModeData,
-                ProgramCounterWithDisplacementModeData,
-                ProgramCounterWithIndexModeData>  sourceOperand;
 };
 
 
