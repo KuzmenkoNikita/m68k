8b2b4cd6cc49c2ec7c75dfd5ca203aa0b5d1b42f
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2026-08-08 09:32:48 +0000

    ROR decoders

---
+++ src/devices/CPU/src/decoders/ROR_register_decoder.cpp
@@ -0,0 +1,45 @@
+#include <bus_helper/bus_helper.h>
+#include <expected>
+#include <instruction_decoder/decoders/ROR_register_decoder.h>
+#include <instruction_decoder/decoders/decoders_helpers.h>
+
+namespace m68k::decoders_ {
+
+namespace {
+
+constexpr uint16_t REGISTER_MASK = 0b111U;
+constexpr uint16_t IR_MASK = 0b10'0000U;
+constexpr uint16_t SIZE_MASK = 0b1100'0000U;
+constexpr uint16_t COUNT_OR_REGISTER_MASK = 0b1110'0000'0000U;
+
+} //namespace
+
+ROR_Register_Decoder::ROR_Register_Decoder(std::shared_ptr<DataExchange::MemoryInterface> bus) : bus_(std::move(bus))
+{
+
+}
+
+std::expected<DecodeResult, DecodeError> ROR_Register_Decoder::decode(uint16_t opcodeWord, uint32_t instructionStartAddr) const
+{
+    InstructionData::ROR_Register_InstructionData instructionData{};
+
+    instructionData.dataRegisterToBeRotated = (opcodeWord & REGISTER_MASK);
+    instructionData.countOrRegister = (opcodeWord & COUNT_OR_REGISTER_MASK) >> 9U; //NOLINT
+    instructionData.shiftMode = (opcodeWord & IR_MASK) != 0 ?   InstructionData::ROR_Register_InstructionData::RotateMode::REGISTER : 
+                                                                InstructionData::ROR_Register_InstructionData::RotateMode::IMMEDIATE;
+    const auto sizeValue = (opcodeWord & SIZE_MASK) >> 6U; //NOLINT
+
+    switch(sizeValue) {
+        case 0b00: instructionData.size = OperationSize::BYTE; break; 
+        case 0b01: instructionData.size = OperationSize::WORD; break; 
+        case 0b10: instructionData.size = OperationSize::LONG; break; 
+        default: return std::unexpected(DecodeError::INVALID_INSTRUCTION_SIZE);
+    }
+
+    return DecodeResult {
+        .instruction = instructionData,
+        .instructionSizeBytes = static_cast<uint32_t>(sizeof(opcodeWord))
+    };
+}
+
+} // namespace m68k::decoders_
